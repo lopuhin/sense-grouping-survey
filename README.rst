e9af11e09ca27bf8aa69aa5bdcd39b690acb3edb
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-07-29 11:32:06 +0100

    Minimal customizations

---
+++ README.rst
@@ -0,0 +1,19 @@
+Sense grouping survey
+=====================
+
+Install::
+
+    pip install -r requirements.txt
+
+Run migrations::
+
+    ./manage.py migrate
+
+Create superuser::
+
+    ./manage.py createsuperuser
+
+Start development server::
+
+    ./manage.py runserver
+
--- sgs/settings.py
+++ sgs/settings.py
@@ -82,36 +82,13 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'ru-ru'
+TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
