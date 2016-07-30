71fd1e07948337299dfe52b753452903c2106afa
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-07-30 12:49:49 -0400

    Serve static, get secret key from env variables (WIP)

--- sgs/urls.py
+++ sgs/urls.py
@@ -1,18 +1,4 @@
-"""sgs URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -24,4 +10,6 @@ urlpatterns = [
     url(r'^group/([\w\-]+)/(\d+)/$',
         survey.views.survey_step, name='survey_step'),
     url(r'^admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT, 'show_indexes': settings.DEBUG}),
 ]
