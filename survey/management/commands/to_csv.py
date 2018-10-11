f9b317467fd8b779c1ff3bdf904ce9f03256f37c
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2018-10-11 23:19:44 +0300

    Export results to csv

---
+++ survey/management/commands/to_csv.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+
+import pandas as pd
+from django.db import connection
+from django.core.management.base import BaseCommand
+
+from survey.models import Context, ContextGroup
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('main_csv')
+        parser.add_argument('contexts_csv')
+
+    def handle(self, main_csv, contexts_csv, **options):
+        with connection.cursor() as cursor:
+            ctx_ids_by_cg = defaultdict(set)
+            cursor.execute('select contextgroup_id, context_id '
+                           'from survey_contextgroup_contexts')
+            for cg_id, ctx_id in cursor.fetchall():
+                ctx_ids_by_cg[cg_id].add(ctx_id)
+        cgs_data = []
+        for cg in (ContextGroup.objects
+                .select_related('participant', 'context_set')):
+            cgs_data.append({
+                'contexts': ' '.join(map(str, ctx_ids_by_cg[cg.id])),
+                'word': cg.context_set.word,
+                'is_filler': cg.context_set.is_filler,
+                'group': cg.context_set.group,
+                'participant': cg.participant.id,
+                'source': cg.participant.source,
+            })
+        df = pd.DataFrame(
+            cgs_data,
+            columns=['participant', 'source', 'is_filler', 'word', 'group',
+                     'contexts']
+        )
+        df.to_csv(main_csv, index=None)
+        contexts_df = pd.DataFrame(
+            [{'id': c.id, 'derivation': c.derivation, 'text': c.text}
+             for c in Context.objects.all()],
+            columns=['id', 'derivation', 'text'])
+        contexts_df.to_csv(contexts_csv, index=None)
